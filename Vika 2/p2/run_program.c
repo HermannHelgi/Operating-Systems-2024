490af71eba7d694b3bca13da0d08abcb14955b61
AgustM22 <agust22@ru.is>
2024-01-29 12:01:01 +0000

    ngfrtyuijk

--- Vika 2/p2/run_program.c
+++ Vika 2/p2/run_program.c
@@ -43,11 +43,6 @@ int run_program(char *file_path, char *argv[])
                 i++;
             }
             argv_new[i+1] = '\0';
-
-            for (int i = 0; argv_new[i] != NULL; ++i) {
-                printf("argv_new[%d]: %s\n", i, argv[i]);
-            }
-
             execvp(file_path, argv_new);
         }
 
