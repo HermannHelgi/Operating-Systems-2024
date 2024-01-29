6f6c5bec9e6e1e43f90d875836227ad4f744f5b3
AgustM22 <agust22@ru.is>
2024-01-29 10:27:35 +0000

    pushing

--- Vika 2/p2/run_program.c
+++ Vika 2/p2/run_program.c
@@ -1,11 +1,31 @@
 #include "run_program.h"
+#include <stdio.h>
+#include <unistd.h>
 
 #define ERROR_CODE 255
 
 int run_program(char *file_path, char *argv[])
 {
-    (void) file_path;
-    (void) argv;
+
+    pid_t pid;
+    pid = fork();
+    if (pid < 0)
+    {
+        printf("Error creating fork.");
+    }
+    if (pid == 0)
+    {
+        printf("Child doing stuff");
+        execv(file_path, argv);
+    }
+    else
+    {
+        printf("parent waiting");
+        wait();
+        printf("parent done waiting");
+        return 1;
+    }
+
 
     // remember to return ERROR_CODE on error.
     return ERROR_CODE;
