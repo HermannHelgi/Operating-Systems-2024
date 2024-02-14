7fd548009f1d15d23e47d7d99e85c2a9578e7d28
AgustM22 <agust22@ru.is>
2024-02-14 13:46:44 +0000

    first test of malloc

--- Vika5/p1/malloc.c
+++ Vika5/p1/malloc.c
@@ -73,10 +73,7 @@ void dumpAllocator()
  */
 uint64_t roundUp(uint64_t n)
 {
-	(void) n;
-
-	//TODO: Implement
-	return 0;
+	return ((n + 15) & ~(15));
 }
 
 /*
@@ -91,22 +88,62 @@ uint64_t roundUp(uint64_t n)
  *   the pointer you need to update for removing block from the free-list/
  *   replace block with a new free block starting somewhere within block
  */
-static void * __attribute__ ((unused)) allocate_block(Block **update_next, Block *block, uint64_t new_size) {
-	(void)update_next;
-	(void)block;
-	(void)new_size;
-	// BTW, feel free to remove these lines starting (void)
-	// Their purpose is just to avoid compiler warnings about unused variables
-	//  as long as this function is unimplemented
+// static void * __attribute__ ((unused)) allocate_block(Block **update_next, Block *block, uint64_t new_size) {
+// 	(void)update_next;
+// 	(void)block;
+// 	(void)new_size;
+// 	// BTW, feel free to remove these lines starting (void)
+// 	// Their purpose is just to avoid compiler warnings about unused variables
+// 	//  as long as this function is unimplemented
 
-	// TODO: Implement
-	return NULL;
-}
+// 	// TODO: Implement
+// 	return NULL;
+// }
 
 
 void *my_malloc(uint64_t size)
 {
-	(void) size;
+	uint64_t totalSize = 16 + roundUp(size);
+	Block *currentBlock = _firstFreeBlock;
+	Block *previousBlock = NULL;
+	while (currentBlock != NULL && currentBlock->size < totalSize )
+	{
+		previousBlock = currentBlock;
+		currentBlock = currentBlock->next;
+	}
+	
+	if (currentBlock != NULL)
+	{
+		if (currentBlock->size != totalSize)
+		{
+			if (previousBlock != NULL)
+			{
+				previousBlock->next = (Block*)((uintptr_t)currentBlock + totalSize);
+				previousBlock->next->next = currentBlock->next;
+				previousBlock->next->size = (currentBlock->size - totalSize);
+			}
+			else
+			{
+				_firstFreeBlock = (Block*)((uintptr_t)currentBlock + totalSize);
+				_firstFreeBlock->next = currentBlock->next;
+				_firstFreeBlock->size = (currentBlock->size - totalSize);
+			}
+		}
+		else
+		{
+			if (previousBlock != NULL)
+			{
+				previousBlock->next = currentBlock->next;
+			}
+			else
+			{
+				_firstFreeBlock = currentBlock->next;
+			}
+		}
+		currentBlock->magic = ALLOCATED_BLOCK_MAGIC;
+		currentBlock->size = totalSize;
+		return currentBlock->data;
+	}
 
 	// TODO: Implement
 	// Suggested approach: Search for a free block, then call allocate_block with that block
@@ -124,17 +161,58 @@ void *my_malloc(uint64_t size)
  */
 static void __attribute__ ((unused)) merge_blocks(Block *block1, Block *block2)
 {
-	(void)block1;
-	(void)block2;
-	// TODO: Implement
+	// Only takes in free blocks.
+	block1->size = block1->size + block2->size;
+	block1->next = block2->next;
 }
 
 
 void my_free(void *address)
 {
-	(void) address;
+	if (address == NULL)
+	{
+		return;
+	}
 
-	// TODO: Implement
+	Block *searchBlock = (Block*)&_heapData[0];
+	Block *myBlock = (Block*)((uintptr_t)address - 16);
+	Block *lastFreeBlock = NULL;
+
+	while (searchBlock != myBlock && searchBlock < (Block*)&_heapData[HEAP_SIZE])
+	{
+		if (searchBlock->magic != ALLOCATED_BLOCK_MAGIC)
+		{
+			lastFreeBlock = searchBlock;
+		}
+		searchBlock = (Block*)(((uintptr_t)searchBlock) + searchBlock->size);
+	} // Leaves as soon as currentblock == given address.
+
+	if (searchBlock == myBlock)
+	{
+		if (lastFreeBlock != NULL) // Found a previous freeblock, it's next pointer has to be changed.
+		{
+			myBlock->next = lastFreeBlock->next;
+			lastFreeBlock->next = myBlock;
+		}
+		else // No free block before the given address, _FF has to be updated.
+		{
+			myBlock->next = _firstFreeBlock;
+			_firstFreeBlock = myBlock;
+		}
+
+		// // Merging Free Spaces
+		// searchBlock = ((Block*)((uintptr_t)myBlock + myBlock->size));
+		// if (searchBlock->magic != ALLOCATED_BLOCK_MAGIC) // Infront
+		// {
+		// 	merge_blocks(myBlock, searchBlock);
+		// }
+		// searchBlock = ((Block*)((uintptr_t)lastFreeBlock + lastFreeBlock->size));
+		// if (searchBlock == myBlock) // Behind
+		// {
+		// 	merge_blocks(searchBlock, myBlock);
+		// }
+	}
+	// If the program fails to find the address, it does nothing.
 }
 
 
