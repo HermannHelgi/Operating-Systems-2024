0bd252a11cde03bb5929f6e436fbe2e3428aaa2c
AgustM22 <agust22@ru.is>
2024-02-26 10:29:21 +0000

    C done

--- Vika6/p1/sim-paging.py
+++ Vika6/p1/sim-paging.py
@@ -3,6 +3,8 @@
 #
 # Put your solution into the three functions in this file
 #
+import matplotlib.pyplot as plt 
+
 
 def get_page_list(filename):
     # Expected functionality: Read content of file (valgrind/lackey output), and then
@@ -22,7 +24,31 @@ def get_page_list(filename):
 
 def plot_memory_access(page_access_list, png_file=None, instruction_page_set=None):
 
-    # TODO: Implement (remove this comment before submission if you implemented somthing)
+    number_of_bins = len(page_access_list) // 1000
+    normalized_dictionary = {}
+    count = 0
+    for elem in sorted(page_access_list):
+        if (elem not in normalized_dictionary):
+            normalized_dictionary[elem] = count
+            count += 1
+
+    array2d = [[0 for i in range(len(normalized_dictionary))] for k in range(number_of_bins)]
+
+    for bin, page in enumerate(page_access_list):
+        array2d[bin//1000][normalized_dictionary[page]] = 1
+
+    plt.imshow(array2d, cmap='gray', aspect='auto')
+
+    # Add labels and title for better interpretation
+    plt.xlabel('Page Numbers')
+    plt.ylabel('Bins / Time of Access')
+    plt.title('Memory Access Patterns')
+
+    # Show or save the plot
+    if png_file:
+        plt.savefig(png_file)
+    else:
+        plt.show()
 
     return
 
