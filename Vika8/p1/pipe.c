0b67625f0e85c932015b8c47cb5f03833d527d7d
HermannHelgi <hermannt22@ru.is>
2024-03-06 22:03:45 +0000

    Belived to have compleated P8p1

--- Vika8/p1/pipe.c
+++ Vika8/p1/pipe.c
@@ -9,26 +9,75 @@
 #include <stdio.h>
 #include <string.h>
           
+#define buffer_size 1024
 
 char *get_output(char *argv[])
 {
-    // Create child process
-    int child_pid = fork();
-    if (child_pid == -1) {
-	perror("fork failed");
-	return NULL;
-    } else if (child_pid == 0) {
-        // Replace program
-        execvp(argv[0], argv);
-	perror("execvp failed");
-	// The child process must terminate if execp failed!
-        exit(255);
-    } else {
-	// let's wait for the child to terminate
-	int status;
-        waitpid(child_pid, &status, 0);
-	
-	char *ptr = strdup("Fake output. Functionality not implemented");
-	return ptr;
+
+    int pipefd[2];
+    char buffer[buffer_size];
+    char *ptr = malloc(buffer_size);
+    if (ptr == NULL)
+    {
+        perror("malloc failed");
+        return NULL;
     }
+
+    if (pipe(pipefd) == -1) 
+    {
+        perror("pipe failed");
+        return NULL;
+    }
+
+    int child_pid = fork();
+    if (child_pid == -1)
+    {
+        perror("fork failed");
+        close(pipefd[0]);
+        close(pipefd[1]);
+        return NULL;
+    } 
+    else if (child_pid == 0) 
+    {
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[0]);
+        close(pipefd[1]);
+
+        if(execvp(argv[0], argv) == -1)
+        {
+	        perror("execvp failed");
+            exit(255);
+            return NULL;
+        }
+    }
+    else
+    {
+
+	    int status;
+        waitpid(child_pid, &status, 0);
+        
+        ssize_t bytes_read = read(pipefd[0], buffer, buffer_size);
+        
+        close(pipefd[0]);
+        close(pipefd[1]);
+        
+        if (bytes_read == -1) 
+        {
+            perror("read failed");
+            return NULL;
+        }
+
+
+
+        else 
+        {
+            int i;
+            for (i = 0; i < buffer_size && buffer[i] != '\n'; i++) 
+            {
+                ptr[i] = buffer[i];
+            }
+            ptr[i] = '\0'; // Null-terminate the string
+        }
+    }
+    return ptr;
 }
