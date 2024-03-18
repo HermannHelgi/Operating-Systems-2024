02e4c4c946bfaffdef1456cddad892a3d65e05e9
AgustM22 <agust22@ru.is>
2024-03-18 09:57:05 +0000

    p2 test

--- Vika9/p2/multi_mutex.c
+++ Vika9/p2/multi_mutex.c
@@ -7,15 +7,34 @@
 
 int multi_mutex_unlock(pthread_mutex_t **mutexv, int mutexc)
 {
-    (void) mutexv;
-    (void) mutexc;
+    int error;
+    for (int i = 0; i < mutexc; i++)
+    {
+        error = pthread_mutex_unlock(mutexv[i]);
+    }
+
+    if (error != 0)
+    {
+        return -1;
+    }
     return 0;
 }
 
 int multi_mutex_trylock(pthread_mutex_t **mutexv, int mutexc)
 {
-    (void) mutexv;
-    (void) mutexc;
+    int result;
+    for (int i = 0; i < mutexc; i++)
+    {
+        result = pthread_mutex_trylock(mutexv[i]);
+
+        if (result != 0) {
+            for (int j = 0; j < i; j++)
+            {
+                pthread_mutex_unlock(mutexv[j]);
+            }
+            return -1;
+        }
+    }
+
     return 0;
 }
-
