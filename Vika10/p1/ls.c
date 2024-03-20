8076430aef84eb6a1c3ebf472108453580e0a6f7
AgustM22 <agust22@ru.is>
2024-03-20 11:30:38 +0000

    Template of P10, first test

---
+++ Vika10/p1/ls.c
@@ -0,0 +1,73 @@
+/* 
+ * Group number (on canvas): 21
+ * Student 1 name: Hermann Helgi
+ * Student 2 name: Ágúst Máni
+ */
+
+#include "ls.h"
+
+// You may not need all these headers ...
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int list(const char* path, int recursive)
+{
+	DIR *opened_directory;
+	int error;
+	opened_directory = opendir(path);
+	if (opened_directory == NULL)
+	{
+		return -1;
+	}
+
+	struct dirent *new_file;
+	struct stat new_file_statistics;
+	size_t size;
+	char* full_path_and_name = malloc(sizeof(char) * (MAX_FILE_NAME_LENGTH + strlen(path)));
+	char type_str[MAX_FILE_NAME_LENGTH];
+
+	new_file = readdir(opened_directory);
+	while (new_file != NULL)
+	{
+		strcpy(full_path_and_name, path);
+		strcat(full_path_and_name, new_file->d_name);
+		error = fstatat(opened_directory, path, &new_file_statistics, AT_SYMLINK_NOFOLLOW);
+		if (error != 0)
+		{
+			free(full_path_and_name);
+			return error;
+		}
+		size = new_file_statistics.st_size;
+		
+		if (new_file->d_type == DT_FIFO)
+		{
+			strcpy(type_str, "|");
+		}
+		else if (new_file->d_type == DT_DIR)
+		{
+			strcpy(type_str, "/");
+		}
+		else if (new_file->d_type == DT_LNK)
+		{
+			strcpy(type_str, " -> ");
+			// Not really sure how i should do this...
+		}
+		else if (new_file_statistics.st_mode & 0111)
+		{
+			strcpy(type_str, "*");
+		}
+		
+		_printLine(size, full_path_and_name, 0);
+		new_file = readdir(opened_directory);
+	}
+
+	error = closedir(opened_directory);
+	free(full_path_and_name);
+	return error;
+}
