4c4e895f2afcf352c15f9649fd20ccd8e73ad9dc
AgustM22 <agust22@ru.is>
2024-03-20 13:17:55 +0000

    Bug fixing more

--- Vika10/p1/ls.c
+++ Vika10/p1/ls.c
@@ -54,7 +54,7 @@ int list(const char* path, int recursive)
 		strcpy(new_path, "/");
 
 		// error = fstatat(dirfd(opened_directory), new_path, &new_file_statistics, AT_SYMLINK_NOFOLLOW);
-		error = lstat(full_path_and_name, &new_file_statistics)
+		error = lstat(full_path_and_name, &new_file_statistics);
 		if (error != 0)
 		{
 			free(full_path_and_name);
