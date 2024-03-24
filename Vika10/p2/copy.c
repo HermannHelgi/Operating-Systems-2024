ca2b3dc479a1501f13349615498babd5f8579fbe
Hermann Helgi <hermannt22@ru.is>
2024-03-24 18:20:43 +0000

    Might have fixed problem for final testcase

--- Vika10/p2/copy.c
+++ Vika10/p2/copy.c
@@ -76,7 +76,7 @@ int doCopy(CopyArgs* args)
 	};
 	
 
-	int new_file = open(args->to, O_WRONLY | O_CREAT | O_EXCL, 0777, current_status.st_mode);
+	int new_file = open(args->to, O_WRONLY | O_CREAT | O_EXCL, &current_status, current_status.st_mode);
 	if (new_file == -1) // Fails to create new file
 	{
 		close(source_file);
