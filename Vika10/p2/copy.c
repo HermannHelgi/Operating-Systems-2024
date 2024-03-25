1ce3d2a4ebabbae98bead1c51e27db29f172f12b
Hermann Helgi <hermannt22@ru.is>
2024-03-25 09:45:10 +0000

    Attempting to fix test case 4

--- Vika10/p2/copy.c
+++ Vika10/p2/copy.c
@@ -54,7 +54,7 @@ int parseCopyArgs(int argc, char * const argv[], CopyArgs* args)
 int doCopy(CopyArgs* args)
 {
 
-	char my_buffer[args->blocksize];
+	char my_buffer[args->blocksize*8];
 	int bytes_read;
 	int bytes_written;
 
@@ -96,7 +96,7 @@ int doCopy(CopyArgs* args)
         }
 		if (empty_block) 
 		{
-            if (lseek(new_file, sizeof(my_buffer), SEEK_CUR) == -1) 
+            if (lseek(new_file, args->blocksize, SEEK_CUR) == -1) 
 			{
                 close(source_file);
                 close(new_file);
