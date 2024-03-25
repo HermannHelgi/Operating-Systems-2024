9f27adf92b567b20184c54f9ab40b424325bf650
Hermann Helgi <hermannt22@ru.is>
2024-03-25 10:47:58 +0000

    Attempting to fix test case 4

--- Vika10/p2/copy.c
+++ Vika10/p2/copy.c
@@ -97,7 +97,7 @@ int doCopy(CopyArgs* args)
 		if (empty_block) 
 		{
 			
-            if (lseek(source_file, args->blocksize, SEEK_CUR) == -1) 
+            if (lseek(new_file, args->blocksize*args->blocksize-1, SEEK_CUR) == -1) 
 			{
                 close(source_file);
                 close(new_file);
