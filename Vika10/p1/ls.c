045aec3ceb489ce536abf5dd2bde03d6b9e98d08
AgustM22 <agust22@ru.is>
2024-03-25 11:15:40 +0000

    testing p1

--- Vika10/p1/ls.c
+++ Vika10/p1/ls.c
@@ -81,7 +81,7 @@
 			{
 				strcpy(type_str, "");
 			}
-			else if (new_file_statistics.st_mode == S_IXUSR)
+			else if (new_file_statistics.st_mode & S_IXUSR)
 			{
 				strcpy(type_str, "*");
 			}
